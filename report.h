9c45b16b0d98071d6417b47bc35bf4599d5de5a8
Sab811 <u3658910@connect.hku.hk>
2026-05-02 19:30:24 +0800

    Delete report.h

--- report.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-// report.h - Monthly report module
-// Handles statistical analysis and ASCII charts.
-
-#include <string>
-
-namespace Report {
-    void run(const std::string& userId);
-}
